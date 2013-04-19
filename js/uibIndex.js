7cb0f44481dda9cc7ae5d03bcad977c4a8ff9bd8
Federico Freire <nerdscape@gmail.com>
2013-04-19 01:17:21 -0300

    Topic 1: Tweets overlay added. Fixed greeting message service call.

--- js/uibIndex.js
+++ js/uibIndex.js
@@ -1,5 +1,6 @@
 
 var enteredName;
+
 $(document).ready(onDocumentFinishedLoading);
 
 /**
@@ -10,6 +11,7 @@ function onDocumentFinishedLoading (){
   // bind buttons' action
   $('#greeting-button').bind('click', onButtonPressed);
   $('#twitter-button').bind('click', getTweets);
+  $('#close-floating-box').bind('click', hideTweets);
 
   alert('Page has finished loading.');
 
@@ -22,20 +24,22 @@ function onDocumentFinishedLoading (){
 */
 function onButtonPressed(){
   enteredName = $('#alias').val();
-  $.get('http://bootcamp.aws.af.cm/welcome/' + enteredName, onServiceCallResponse).fail(onServiceCallFail);
+  $.ajax({
+    url: 'http://bootcamp.aws.af.cm/welcome/' + enteredName,
+    success: onServiceCallResponse,
+    dataType: 'json'
+  }).fail(onServiceCallFail);
 }
 
 /**
 * Called once the response of the service is get.
 */
 function onServiceCallResponse(data){
-  var responseObject = JSON.parse(data);
-
   // check if the response was successful or not based in its properties
-  if (responseObject.hasOwnProperty('response')) {
-    setGreetMessage(responseObject.response);
-  } else if (responseObject.hasOwnProperty('error')) {
-    setErrorMessage(responseObject.error);
+  if (data.hasOwnProperty('response')) {
+    setGreetMessage(data.response);
+  } else if (data.hasOwnProperty('error')) {
+    setErrorMessage(data.error);
   } else {
     setErrorMessage('Something went wrong...');
   }
@@ -47,12 +51,11 @@ function setErrorMessage(message){
 
 function setGreetMessage(message){
   message = message.replace(enteredName, '<span class="highlight">' + enteredName + '</span>');
-  console.log(message);
   $('#response-container').html(message).removeClass('error');
 }
 
 /**
-* Called when the service call fails (eg. 500 error)
+* Executed when the service call fails (eg. 500 error)
 */
 function onServiceCallFail()
 {
@@ -63,19 +66,52 @@ function onServiceCallFail()
 * Calls Twitter API
 */
 function getTweets(){
-   var serviceUrl = 'http://search.twitter.com/search.json';
-
-   $.ajax({
-    url: serviceUrl,
+  $.ajax({
+    url: 'http://search.twitter.com/search.json',
     data:{'q': 'html5'},
     dataType: 'jsonp',
-    jsonpCallback: onGetTweetsSuccess
-   });
+    jsonpCallback: 'onGetTweetsSuccess'
+  });
 }
 
 /**
 * Callback function for Twitter service AJAX call
 */
 function onGetTweetsSuccess(data){
-  console.log(data);
+  var tweets = data.results;
+  showTweets(tweets);
+}
+
+function showTweets(tweets){
+
+  for (var index in tweets){
+    $('#tweets-container').append(createTweetItem(tweets[index], index));
+  }
+
+  // animate overlay
+  $('.floating-box').show('slow');
+}
+
+function createTweetItem(tweet, index){
+  var oddityClass = (+index % 2 === 0) ? 'even' : 'odd';
+
+  var $avatarImage = $('<img>').attr('src', tweet.profile_image_url);
+  var $avatar = $('<div class="avatar"></div>').html($avatarImage);
+
+  var $user = $('<div class="left"></div>').html(tweet.from_user);
+  var $date = $('<div class="right"></div>').html(tweet.created_at);
+  var $tweetTop = $('<div class="tweet-top-text"></div>').append($user).append($date);
+  var $text = $('<div></div>').html(tweet.text);
+  var $description = $('<div class="tweet-description"></div>').append($tweetTop).append($text);
+
+  var $tweetItem = $('<div class="tweet ' + oddityClass + '"></div>').append($avatar).append($description);
+
+  return $tweetItem;
+}
+
+/**
+* Hides the overlay that contains the tweets
+*/
+function hideTweets(){
+  $('.floating-box').hide('slow');
 }
