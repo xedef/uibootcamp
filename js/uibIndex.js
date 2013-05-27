966e99a9a0a53b670d20cb72a5039064b6546b1e
fefuts <nerdscape@gmail.com>
2013-05-27 19:58:00 -0300

    Topic 2: Points 1 to 9.

--- js/uibIndex.js
+++ js/uibIndex.js
@@ -1,222 +1,270 @@
+var uibIndex = (function($){
 
-var enteredName;
-var twitterDataValues;
-var twitterSearchTopic = 'html5'; // used as a constant
-var previousTweetsContainerHeight = 0;
+  var enteredName,
+      twitterDataValues,
+      TWITTER_SEARCH_TOPIC = 'html5',
+      previousTweetsContainerHeight = 0;
 
-// call the initializing function once the page loads
-window.onload = onDocumentFinishedLoading;
+  // call the initializing function once the page loads
+  // window.onload = onDocumentFinishedLoading;
 
-/**
-* Executed when the page finishes loading.
-*/
-function onDocumentFinishedLoading (){
+  /**
+  * Executed when the page finishes loading.
+  */
+  var init = function (){
 
-  $('#twitter-button').val('Let\'s see what people are saying about ' + twitterSearchTopic);
+    $('#home').bind('pageinit', initHomePage);
 
-  // bind buttons' action
-  $('#greeting-button').bind('click', onButtonPressed);
-  $('#twitter-button').bind('click', getTweets);
-  $('#close-floating-box').bind('click', hideTweets);
-  $('#get-more-tweets').bind('click', getMoreTweets);
+    // bind buttons' action
+    $('#greeting-button').bind('click', onGreetingButtonPressed);
+    $('#twitter-button').bind('click', getTweets);
+    $('#get-more-tweets').bind('click', getMoreTweets);
+    $('#show-menu').bind('click', showMenu);
 
-  alert('Page has finished loading.');
+    alert('Page has finished loading.');
 
-  // set focus on the textbox
-  $('#alias').focus();
+    // set focus on the textbox
+    $('#alias').focus();
 
-  // initialize Twitter API call data parameters
-  twitterDataValues = {'q': twitterSearchTopic};
-}
+    // initialize Twitter API call data parameters
+    twitterDataValues = {'q': TWITTER_SEARCH_TOPIC};
 
-/**
-* Executed when the button in the templated is pressed.
-*/
-function onButtonPressed(){
-  enteredName = $('#alias').val();
-  var url = 'http://bootcamp.aws.af.cm/welcome/' + enteredName;
+    // bind ESC key functionality
+    $(document).on('keydown',
+      function(e){
+        if(+e.which === 27){
+          hideLoader();
+        }
+      }
+    );
 
-  // Thanks, IE!
-  if (window.XDomainRequest) {
-    // Use Microsoft XDR
-    var xdr = new XDomainRequest();
-    xdr.open('get', url);
-    xdr.onload = function() {
-        onServiceCallResponse(JSON.parse(xdr.responseText));
-    };
-    xdr.send();
-  } else { // decent browsers...
+    $( '#show-gallery-image' ).bind( 'click', onShowGalleryImage );
+  };
+
+  var initHomePage = function(){
+    $('#twitter-button').find('.ui-btn-text').html('Get ' + TWITTER_SEARCH_TOPIC + ' tweets');
+  };
+
+  /**
+  * Executed when the button in the templated is pressed.
+  */
+  var onGreetingButtonPressed = function (){
+    enteredName = $('#alias').val();
+    var url = 'http://bootcamp.aws.af.cm/welcome/' + enteredName;
+
+    // Thanks, IE!
+    if (window.XDomainRequest) {
+      // Use Microsoft XDR
+      var xdr = new XDomainRequest();
+      xdr.open('get', url);
+      xdr.onload = function() {
+          onServiceCallResponse(JSON.parse(xdr.responseText));
+      };
+      xdr.send();
+    } else { // decent browsers...
+      $.ajax({
+        url: url,
+        dataType : 'json',
+        success: onServiceCallResponse
+      }).fail(onServiceCallFail);
+    }
+  };
+
+
+  /**
+  *
+  */
+  var showMenu = function (){
+    $('#menu-panel').panel('open');
+  };
+
+  /**
+  * Called once the response of the service is get.
+  */
+  var onServiceCallResponse = function (data){
+    // check if the response was successful or not based in its properties
+    if (data.hasOwnProperty('response')) {
+      setGreetMessage(data.response);
+    } else if (data.hasOwnProperty('error')) {
+      setErrorMessage(data.error);
+    } else {
+      setErrorMessage('Something went wrong...');
+    }
+  };
+
+  var setErrorMessage = function (message){
+    $('#response-container').html(message).addClass('error');
+  };
+
+  var setGreetMessage = function (message){
+    message = message.replace(enteredName, '<span class="highlight">' + enteredName + '</span>');
+    $('#response-container').html(message).removeClass('error');
+  };
+
+  /**
+  * Executed when the service call fails (eg. 500 error)
+  */
+  var onServiceCallFail = function(){
+    $('#response-container').html('Service call failed!').addClass('error');
+  };
+
+  /**
+  * Calls Twitter API
+  */
+  var getTweets = function (){
+
+    showLoader();
+
     $.ajax({
-      url: url,
-      success: onServiceCallResponse,
-      dataType : 'json'
-    }).fail(onServiceCallFail);
-  }
-}
+      url: 'http://search.twitter.com/search.json',
+      data: twitterDataValues,
+      dataType: 'jsonp',
+      success: onGetTweetsSuccess
+    });
+  };
 
-/**
-* Called once the response of the service is get.
-*/
-function onServiceCallResponse(data){
-  // check if the response was successful or not based in its properties
-  if (data.hasOwnProperty('response')) {
-    setGreetMessage(data.response);
-  } else if (data.hasOwnProperty('error')) {
-    setErrorMessage(data.error);
-  } else {
-    setErrorMessage('Something went wrong...');
-  }
-}
+  /**
+  * Same as "getTweets" but checking/setting the "More Tweets" button status
+  */
+  var getMoreTweets = function (){
 
-function setErrorMessage(message){
-  $('#response-container').html(message).addClass('error');
-}
+    // where do you think you're going?
+    if ($('#get-more-tweets').hasClass('disabled')){
+      return;
+    }
 
-function setGreetMessage(message){
-  message = message.replace(enteredName, '<span class="highlight">' + enteredName + '</span>');
-  $('#response-container').html(message).removeClass('error');
-}
+    setMoreTweetsButtonEnabledStatus(false);
+    getTweets();
+  };
 
-/**
-* Executed when the service call fails (eg. 500 error)
-*/
-function onServiceCallFail()
-{
-  $('#response-container').html('Service call failed!').addClass('error');
-}
+  /**
+  * Callback function for Twitter service AJAX call
+  */
+  var onGetTweetsSuccess = function(data){
+    var tweets = data.results;
 
-/**
-* Calls Twitter API
-*/
-function getTweets(){
+    parseNextPageDataValues(data.next_page);
+    addTweetsToContainer(tweets);
 
-  $.ajax({
-    url: 'http://search.twitter.com/search.json',
-    data: twitterDataValues,
-    dataType: 'jsonp',
-    jsonpCallback: 'onGetTweetsSuccess'
-  });
-}
+    hideLoader();
 
-/**
-* Same as "getTweets" but checking/setting the "More Tweets" button status
-*/
-function getMoreTweets(){
+    // save the current lastItem position for the next page scrolling
+    // previousTweetsContainerHeight = $('.floating-box-content')[0].scrollHeight;
 
-  // where do you think you're going?
-  if ($('#get-more-tweets').hasClass('disabled')){
-    return;
-  }
+    //setMoreTweetsButtonEnabledStatus(true);
+  };
 
-  setMoreTweetsButtonEnabledStatus(false);
-  getTweets();
-}
+  var showLoader = function(){
+    $.mobile.loading( 'show', {
+        textVisible: false,
+        theme: 'a',
+        textonly: false
+    });
+  };
 
-/**
-* Callback function for Twitter service AJAX call
-*/
-function onGetTweetsSuccess(data){
-  var tweets = data.results;
+  var hideLoader = function(){
+    $.mobile.loading('hide');
+  };
 
-  parseNextPageDataValues(data.next_page);
-  addTweetsToContainer(tweets);
+  /**
+  * Sets the status of the "More Twweets" button to enabled or disabled
+  */
+  var setMoreTweetsButtonEnabledStatus = function(enabledValue){
+    if (enabledValue) {
+      $('#get-more-tweets').removeClass('disabled');
+    } else {
+      $('#get-more-tweets').addClass('disabled');
+    }
+  };
 
-  // save the current lastItem position for the next page scrolling
-  previousTweetsContainerHeight = $('.floating-box-content')[0].scrollHeight;
+  /**
+  * Parses the "next_page" value from Twitter response to identify the parameters for the next call
+  */
+  var parseNextPageDataValues = function(queryString){
+    // ?page=2&max_id=324969446781370368&q=html5
+    var parameters;
+    var newTwitterDataValues = {};
+    var kvPair;
 
-  setMoreTweetsButtonEnabledStatus(true);
-}
+    // drop initial question mark if present
+    if (queryString[0] == '?') {
+      queryString = queryString.substring(1);
+    }
 
-/**
-* Sets the status of the "More Twweets" button to enabled or disabled
-*/
-function setMoreTweetsButtonEnabledStatus(enabledValue){
-  if (enabledValue) {
-    $('#get-more-tweets').removeClass('disabled');
-  } else {
-    $('#get-more-tweets').addClass('disabled');
-  }
+    // split queries
+    parameters = queryString.split('&');
 
-}
+    // split parameters/values in pairs
+    for (var index in parameters) {
+      kvPair = parameters[index].split('=');
+      newTwitterDataValues[kvPair[0]] = kvPair[1];
+    }
 
-/**
-* Parses the "next_page" value from Twitter response to identify the parameters for the next call
-*/
-function parseNextPageDataValues(queryString){
-  // ?page=2&max_id=324969446781370368&q=html5
-  var parameters;
-  var newTwitterDataValues = {};
-  var kvPair;
+    twitterDataValues = newTwitterDataValues;
+  };
 
-  // drop initial question mark if present
-  if (queryString[0] == '?') {
-    queryString = queryString.substring(1);
-  }
+  /**
+  * Adds the tweets that came in the response to the container.
+  */
+  var addTweetsToContainer = function(tweets){
 
-  // split queries
-  parameters = queryString.split('&');
+    for (var index in tweets){
+      $('#tweets-container').append(createTweetListViewItem(tweets[index]));
+    }
 
-  // split parameters/values in pairs
-  for (var index in parameters) {
-    kvPair = parameters[index].split('=');
-    newTwitterDataValues[kvPair[0]] = kvPair[1];
-  }
-
-  twitterDataValues = newTwitterDataValues;
-}
-
-/**
-* Adds the tweets that came in the response to the container.
-*/
-function addTweetsToContainer(tweets){
-
-  for (var index in tweets){
-    $('#tweets-container').append(createTweetItem(tweets[index], index));
-  }
-
-  // animate overlay
-  if ($('.floating-box').hasClass('hidden')){
+    $('#tweets-container').listview('refresh');
     showTweets();
-  }
 
-  if (previousTweetsContainerHeight) {
-    $('.floating-box-content').animate({scrollTop: previousTweetsContainerHeight}, 500);
-  }
-}
+    if (previousTweetsContainerHeight) {
+      $('.floating-box-content').animate({scrollTop: previousTweetsContainerHeight}, 500);
+    }
+  };
 
-/**
-* Creates a tweet template item that is going to be shown in the container
-*/
-function createTweetItem(tweet, index){
-  var oddityClass = (+index % 2 === 0) ? 'even' : 'odd';
+  /**
+  * Creates and returns a listviewitem that contains a single tweet information
+  */
+  var createTweetListViewItem = function(tweet){
+    var $lviTweet = $('<li></li>');
 
-  var $avatarImage = $('<img>').attr('src', tweet.profile_image_url);
-  var $avatar = $('<div class="avatar"></div>').html($avatarImage);
+    $date = $('<p class="ui-li-aside"></p>').append($('<strong></strong>').html(formatTweetDate(tweet.created_at)));
+    $lviTweet.append($('<img>').attr('src', tweet.profile_image_url));
+    $lviTweet.append($date);
+    $lviTweet.append($('<h2></h2>').html(tweet.from_user));
+    $lviTweet.append($('<p></p>').html(tweet.text));
 
-  var $user = $('<div class="left"></div>').html(tweet.from_user);
-  var $date = $('<div class="right"></div>').html(tweet.created_at);
-  var $tweetTop = $('<div class="tweet-top-text"></div>').append($user).append($date);
-  var $text = $('<div></div>').html(tweet.text);
-  var $description = $('<div class="tweet-description"></div>').append($tweetTop).append($text);
+    return $lviTweet;
+  };
 
-  var $tweetItem = $('<div class="tweet ' + oddityClass + '"></div>').append($avatar).append($description);
+  /**
+  * Returns a formatted date based in the tweet "created_at"
+  */
+  var formatTweetDate = function(date){
+    // the easiest slappiest
+    return date.replace(' +0000', '');
+  };
 
-  return $tweetItem;
-}
+  /**
+  * Hides the overlay that contains the tweets
+  */
+  var showTweets = function(){
+    $('.floating-box').popup('open');
+  };
 
-/**
-* Hides the overlay that contains the tweets
-*/
-function showTweets(){
-  $('.overlay-background').fadeIn('slow');
-  $('.floating-box').show('slow');
-}
+  var onShowGalleryImage = function(){
+    showLoader();
+    var galleryImage = new Image();
+    galleryImage.onload = onGalleryImageFinishedLoading;
+    galleryImage.src = 'http://nukethefridge.com/wp-content/uploads/2013/02/back-to-the-future-delorean.jpg';
+  };
 
+  var onGalleryImageFinishedLoading = function(){
+    $(this).css('display', 'none');
+    $('#gallery-page div[data-role=content] .gallery-image-container').html($(this));
+    $(this).fadeIn('slow');
+    hideLoader();
+  };
 
-/**
-* Hides the overlay that contains the tweets
-*/
-function hideTweets(){
-  $('.overlay-background').fadeOut('slow');
-  $('.floating-box').hide('slow');
-}
+  return init();
+});
+
+jQuery(document).ready(uibIndex);
