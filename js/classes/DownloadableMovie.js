9c42d3ee1d065da5594f145b292a4d8eb91d9717
fefuts <nerdscape@gmail.com>
2013-06-03 00:41:28 -0300

    FIX: Unused code removed.

--- js/classes/DownloadableMovie.js
+++ js/classes/DownloadableMovie.js
@@ -12,11 +12,3 @@ var DownloadableMovieModule = Object.create(MovieModule);
 DownloadableMovieModule.download = function(){
   console.log('downloading ' + this.get('title')) ;
 };
-
-
-var M = Object.create(null);
-M.play = function(){return 'play';};
-
-var N = Object.create(M);
-N.stop = function(){return 'play';};
-
