2672fc276410498cf6b11a4edb0019e8dab1f2c6
fefuts <federico.freire@glb394.ea.internal.globant>
2013-06-04 17:34:24 -0300

    Topic 4: Points 7 and 8 completed.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-require(['requireFiles/Movie', 'requireFiles/Director'], function(Movie, Director) {
+requirejs.config({
+  paths: {
+    'jquery': 'thirdparty/jquery-1.9.1',
+    'jquery.mobile': 'thirdparty/jquery.mobile-1.3.1'
+  },
+  shim: {
+    'jqueryMobile': {
+      deps: ['jquery']
+    }
+  }
+});
+
+require(['requireFiles/Movie', 'requireFiles/Director', 'jquery', 'jquery.mobile'], function(Movie, Director) {
+  var m  = new Movie();
+  m.get('title');
 
 });
